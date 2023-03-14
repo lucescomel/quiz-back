425b74765bb9567c008c86fd0ba02b7af568b57a
Luc <lucescomel@gmail.com>
2023-03-14 10:14:47 +0100

    add: Answer and feature: relation question/answer

---
+++ src/Entity/Answers.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnswersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnswersRepository::class)]
+class Answers
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\ManyToOne(inversedBy: 'answers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Questions $id_question = null;
+
+    #[ORM\OneToOne(mappedBy: 'id_success', cascade: ['persist', 'remove'])]
+    private ?Questions $questions = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIdQuestion(): ?Questions
+    {
+        return $this->id_question;
+    }
+
+    public function setIdQuestion(?Questions $id_question): self
+    {
+        $this->id_question = $id_question;
+
+        return $this;
+    }
+
+    public function getQuestions(): ?Questions
+    {
+        return $this->questions;
+    }
+
+    public function setQuestions(Questions $questions): self
+    {
+        // set the owning side of the relation if necessary
+        if ($questions->getIdSuccess() !== $this) {
+            $questions->setIdSuccess($this);
+        }
+
+        $this->questions = $questions;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Questions.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QuestionsRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QuestionsRepository::class)]
+class Questions
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $title = null;
+
+    #[ORM\OneToMany(mappedBy: 'id_question', targetEntity: Answers::class, orphanRemoval: true)]
+    private Collection $answers;
+
+    #[ORM\OneToOne(inversedBy: 'questions', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Answers $id_success = null;
+
+    public function __construct()
+    {
+        $this->answers = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Answers>
+     */
+    public function getAnswers(): Collection
+    {
+        return $this->answers;
+    }
+
+    public function addAnswer(Answers $answer): self
+    {
+        if (!$this->answers->contains($answer)) {
+            $this->answers->add($answer);
+            $answer->setIdQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAnswer(Answers $answer): self
+    {
+        if ($this->answers->removeElement($answer)) {
+            // set the owning side to null (unless already changed)
+            if ($answer->getIdQuestion() === $this) {
+                $answer->setIdQuestion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getIdSuccess(): ?Answers
+    {
+        return $this->id_success;
+    }
+
+    public function setIdSuccess(Answers $id_success): self
+    {
+        $this->id_success = $id_success;
+
+        return $this;
+    }
+}
